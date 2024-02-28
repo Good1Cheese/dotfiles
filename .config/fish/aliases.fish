8c889be448da4b34c2cca1718456836d1d0292fe
Good1Cheese <2916675@gmail.com>
2024-02-28 17:12:16 +0300

    :inbox-tray: Added clang formatting options, some aliases

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,17 +1,21 @@
-alias nconf='cd ~/.config/nvim'
-alias proj='cd ~/Projects'
-alias aws='cd ~/.config/awesome'
-alias fide='cd ~/.config/fish'
-alias lef='cd ~/.config/lf'
+alias nconf="cd ~/.config/nvim"
+alias proj="cd ~/Projects"
+alias aws="cd ~/.config/awesome"
+alias fide="cd ~/.config/fish"
+alias lef="cd ~/.config/lf"
 
-alias lg='lazygit'
-alias lf='lfcd'
-alias sx='startx'
-alias qq='devour neovide --no-fork'
+alias lg="lazygit"
+alias lf="lfcd"
+alias sx="startx"
+alias umnt="fusermount -u"
+alias phonemnt="aft-mtp-mount"
+alias qq="devour neovide --no-fork"
 # devour neovide --no-fork
 
-alias sudo='doas'
-alias pac='doas pacman'
-alias std='shutdown now'
-alias ssp='systemctl suspend'
-alias knv='killall neovide'
+alias sudo="doas"
+alias pac="doas pacman"
+alias aurupt="yay -Syyu"
+alias upt="doas pacman -Syu"
+alias std="shutdown now"
+alias ssp="systemctl suspend"
+alias knv="killall"
